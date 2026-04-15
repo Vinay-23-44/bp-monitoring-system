db509d15547fda6d42bc428c23e96e82b31ff51c
Vinay Dahiya <kumarvinaydahiya004@gmail.com>
2026-04-15 20:57:29 +0530

    updated axios baseURL for deployment

--- Client/src/api/axios.jsx
+++ Client/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost:4444",
+    baseURL:import.meta.env.VITE_API_URL,
     //baseURL:"https://uncubically-unwormy-krystal.ngrok-free.dev",
     headers:{
         Authorization:`Bearer ${localStorage.getItem("token") || ""}`,
